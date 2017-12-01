fcf99d91ac49e5322dd1908aae7e072aea19d199
Sharon Desmond <mathemagiks@gmail.com>
2017-12-01 23:30:54 +0000

    Access videos on state

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDtRnn9HErYyKhbcIhI4n9BYMfQZo9XBHs';
 
-YTSearch({key: API_KEY, term: 'painbastard'}, function(data){
-	console.log(data);
-});
+
 // Create a new Component. This component should 
 // produce some html.
 class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'painbastard'}, (videos) => {
+			this.setState({ videos });
+			// this.setState({ videos: videos});
+        });
+	}
 	render(){
 		return (
 	      <div>
